77fcc718ac4f04ef88a4758d26b80ca2c3197e0e
bonanza-olaf <48622695+bonanza-olaf@users.noreply.github.com>
2021-06-26 09:59:14 +0900

    [update]globalテーマの設定まで

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">Button</Button>
+      <p>This is samaple.</p>
     </ChakraProvider>
   );
 }
